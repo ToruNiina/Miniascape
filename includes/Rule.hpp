3db4b940036cb4d07e73d4bf26f77efd406814a5
ToruNiina <niina.toru.68u@gmail.com>
2016-07-28 19:03:42 +0900

    add Rule

---
+++ includes/Rule.hpp
@@ -0,0 +1,27 @@
+#ifndef MINIASCAPE_RULE
+#define MINIASCAPE_RULE
+
+namespace miniascape
+{
+
+template<typename T_traits>
+class RuleBase
+{
+  public:
+    using traits_type = T_traits;
+    using time_type   = typename traits_type::time_type;
+    using state_type  = typename traits_type::state_type;
+    using cell_type   = typename traits_type::cell_type;
+
+  public:
+
+    RuleBase() = default;
+    virtual ~RuleBase() = default;
+
+    virtual state_type step(const cell_type& cell) const = 0;
+    virtual time_type  delta_t() const = 0;
+};
+
+}
+
+#endif /* MINIASCAPE_RULE */
