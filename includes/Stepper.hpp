d29e1f4659b970e99a59523a3cf8fa17b009824a
ToruNiina <niina.toru.68u@gmail.com>
2016-07-31 14:21:11 +0900

    add buffer to stepper

--- includes/Stepper.hpp
+++ includes/Stepper.hpp
@@ -1,6 +1,6 @@
 #ifndef MINIASCAPE_STEPPER
 #define MINIASCAPE_STEPPER
-#include "Space.hpp"
+#include "World.hpp"
 #include "Rule.hpp"
 #include "zip_iterator.hpp"
 
@@ -16,21 +16,23 @@ class Stepper
     using state_type  = typename traits_type::state_type;
     using cell_type   = typename traits_type::cell_type;
     using rule_type   = RuleBase<traits_type>;
-    using space_type  = Space<traits_type>;
+    using world_type  = World<traits_type>;
 
-    static time_type step(space_type& space, const rule_type& rule);
+    static time_type step(world_type& world, const rule_type& rule);
 };
 
 template<typename T_traits>
 typename Stepper<T_traits>::time_type
-Stepper<T_traits>::step(space_type& space, const rule_type& rule)
+Stepper<T_traits>::step(world_type& world, const rule_type& rule)
 {
-    for(auto cell = make_zip(space.world().cbegin(),space.temp().begin());
-            cell != make_zip(space.world().cend(),  space.temp().end()); ++cell)
+    std::vector<state_type> temp(world.size());
+
+    for(auto cell = make_zip(world.cbegin(), temp.begin());
+            cell != make_zip(world.cend(),   temp.end()); ++cell)
         *get<1>(cell) = rule.step(**get<0>(cell));
 
-    for(auto cell = make_zip(space.world().begin(), space.temp().begin());
-            cell != make_zip(space.world().end(),   space.temp().end()); ++cell)
+    for(auto cell = make_zip(world.begin(), temp.begin());
+            cell != make_zip(world.end(),   temp.end()); ++cell)
         (*get<0>(cell))->state = *get<1>(cell);
 
     return rule.delta_t();
