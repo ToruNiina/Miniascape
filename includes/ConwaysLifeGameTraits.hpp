bc4f720e06dc640f8e244ca780ff152396c3486c
ToruNiina <niina.toru.68u@gmail.com>
2016-07-31 15:00:38 +0900

    add ConwaysLifeGameTraits

---
+++ includes/ConwaysLifeGameTraits.hpp
@@ -0,0 +1,78 @@
+#ifndef MINIASCAPE_CONWAYS_LIFE_GAME_TRAITS
+#define MINIASCAPE_CONWAYS_LIFE_GAME_TRAITS
+#include "Cell.hpp"
+#include "Rule.hpp"
+#include "Observer.hpp"
+#include "NeighborhoodIndex.hpp"
+#include "PeriodicBoundary.hpp"
+#include "RandomStateGenerator.hpp"
+
+namespace miniascape
+{
+
+struct boolean
+{
+    boolean() : val(false){}
+    boolean(const bool b) : val(b){}
+    bool val;
+
+    operator bool      &()       {return val;}
+    operator bool const&() const {return val;}
+};
+
+struct ConwaysLifeGameTraits
+{
+    using size_type     = std::size_t;
+    using time_type     = std::size_t;
+    using state_type    = boolean;
+    using cell_type     = Cell<8, state_type>;
+    using neighbor_type = MooreNeighborhood;
+    using boundary_type = PeriodicBoundary<neighbor_type>;
+};
+
+using ConwaysLifeGameObserver = Observer<ConwaysLifeGameTraits>;
+
+class ConwaysLifeGameRule : public RuleBase<ConwaysLifeGameTraits>
+{
+  public:
+    using base_type  = RuleBase<ConwaysLifeGameTraits>;
+    using time_type  = typename base_type::time_type;
+    using state_type = typename base_type::state_type;
+    using cell_type  = typename base_type::cell_type;
+
+  public:
+    ConwaysLifeGameRule() = default;
+    ~ConwaysLifeGameRule() override = default;
+
+    state_type step(const cell_type& cell) const override;
+    time_type  delta_t() const override {return 1;}
+};
+
+inline typename ConwaysLifeGameRule::state_type
+ConwaysLifeGameRule::step(const cell_type& cell) const
+{
+    std::size_t lives = 0;
+    for(auto iter = cell.neighbors.cbegin();
+            iter != cell.neighbors.cend(); ++iter)
+        if((*iter)->state.val) ++lives;
+
+    return (cell.state) ? (lives == 2 or lives == 3) : (lives == 3);
+}
+
+template<>
+class RandomStateGenerator<boolean>
+{
+  public:
+    explicit RandomStateGenerator(const unsigned int s) : mt_(s), bn_(0.5){}
+    ~RandomStateGenerator() = default;
+
+    boolean operator()(){return bn_(mt_);}
+
+    std::mt19937 mt_;
+    std::bernoulli_distribution bn_;
+};
+
+
+}
+
+#endif /* MINIASCAPE_CONWAYS_LIFE_GAME_TRAITS */
