def6bd0c4104963665a8309eac4302ab62eb6c5f
ToruNiina <niina.toru.68u@gmail.com>
2016-07-31 15:01:33 +0900

    add Conway's LifeGame

---
+++ src/conways_life_game.cpp
@@ -0,0 +1,21 @@
+#include "ConwaysLifeGameTraits.hpp"
+#include "ConwaysLifeGameSimulator.hpp"
+using namespace miniascape;
+
+int main()
+{
+    ConwaysLifeGameSimulator simu(100, 100, 100);
+    ConwaysLifeGameRule rule;
+    ConwaysLifeGameObserver obs;
+
+    simu.initialize();
+
+    while(simu.step(rule))
+    {
+        simu.observe(std::cout, obs);
+    }
+
+    simu.finalize();
+
+    return EXIT_SUCCESS;
+}
