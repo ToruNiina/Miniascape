c21e85c8344a5ab04316030f21643096498a8730
ToruNiina <niina.toru.68u@gmail.com>
2016-08-01 17:42:49 +0900

    change template argument of Simulator and rename step to step_

--- includes/core/Simulator.hpp
+++ includes/core/Simulator.hpp
@@ -7,33 +7,33 @@
 namespace miniascape
 {
 
-template<template<typename T>class T_world, typename T_traits>
+template<template<typename T>class T_simulator_traits, typename T_traits>
 class Simulator
 {
   public:
-    using traits_type   = T_traits;
-    using time_type     = typename traits_type::time_type;
-    using state_type    = typename traits_type::state_type;
-    using cell_type     = typename traits_type::cell_type;
-    using stepper_type  = Stepper<traits_type>;
-    using rule_type     = RuleBase<traits_type>;
-    using observer_type = Observer<traits_type>;
-    using world_base    = World<traits_type>;
-    using world_type    = T_world<traits_type>;
-
-    static_assert(std::is_base_of<world_base, world_type>::value,
-                  "invalid world type");
-
+    using traits_type      = T_traits;
+    using simulator_traits = T_simulator_traits<traits_type>;
+    using time_type        = typename traits_type::time_type;
+    using state_type       = typename traits_type::state_type;
+    using cell_type        = typename traits_type::cell_type;
+    using world_base       = World<traits_type>;
+    using rule_base        = RuleBase<traits_type>;
+    using observer_base    = Observer<traits_type>;
+    using world_type       = typename simulator_traits::world_type;
+    using rule_type        = typename simulator_traits::rule_type;
+    using observer_type    = typename simulator_traits::observer_type;
+    using stepper_type     = typename simulator_traits::stepper_type;
+ 
   public:
     Simulator(const time_type t_end)
         : t_(0), t_end_(t_end)
     {}
-    ~Simulator() = default;
+    virtual ~Simulator() = default;
 
-    virtual void initialize();
-            bool step(const rule_type& rule);
-            void observe(observer_type& obs);
-    virtual void finalize();
+    virtual void initialize(){};
+            bool step_(const rule_type& rule, stepper_type& stepper);
+            void observe(observer_type& obs) const;
+    virtual void finalize(){};
 
   protected:
 
@@ -42,33 +42,21 @@ class Simulator
     world_type world_;
 };
 
-template<template<typename T>class T_world, typename T_traits>
-inline void Simulator<T_world, T_traits>::initialize()
+template<template<typename T>class T_simulator_traits, typename T_traits>
+inline bool Simulator<T_simulator_traits, T_traits>::step_(
+        const rule_type& rule, stepper_type& stepper)
 {
-    return;
-}
-
-template<template<typename T>class T_world, typename T_traits>
-inline bool Simulator<T_world, T_traits>::step(const rule_type& rule)
-{
-    this->t_ += stepper_type::step(world_, rule);
+    this->t_ += stepper.step(world_, rule);
     return (t_ >= t_end_) ? false : true;
 }
 
-template<template<typename T>class T_world, typename T_traits>
-inline void
-Simulator<T_world, T_traits>::observe(observer_type& obs)
+template<template<typename T>class T_simulator_traits, typename T_traits>
+inline void Simulator<T_simulator_traits, T_traits>::observe(observer_type& obs) const
 {
     obs.observe(this->t_, this->world_);
     return;
 }
 
-template<template<typename T>class T_world, typename T_traits>
-inline void Simulator<T_world, T_traits>::finalize()
-{
-    return;
-}
-
-}
+}//miniascape
 
 #endif /* MINIASCAPE_SIMULATOR */
