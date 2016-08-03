1f3fd55ba2cea5caae4393b6d22e3c6ff8927450
ToruNiina <niina.toru.68u@gmail.com>
2016-08-03 14:16:09 +0900

    add RandomNumberGenerator

--- includes/core/RandomNumberGenerator.hpp
+++ includes/core/RandomNumberGenerator.hpp
@@ -1,9 +1,33 @@
 #ifndef MINIASCAPE_RANDOM_STATE_GENERATOR
 #define MINIASCAPE_RANDOM_STATE_GENERATOR
+#include "util/meta_function.hpp"
 #include <random>
 
 namespace miniascape
 {
+
+class RandomNumberGenerator
+{
+  public:
+    RandomNumberGenerator(const unsigned int seed): seed_(seed), mt_(seed){}
+    ~RandomNumberGenerator() = default;
+
+    template<typename T>
+    T uniform_int(const T begin, const T end)
+    {
+        return (std::uniform_int_distribution<T>(begin, end))(this->mt_);
+    }
+
+    template<typename T>
+    T uniform_real(const T begin, const T end)
+    {
+        return (std::uniform_real_distribution<T>(begin, end))(this->mt_);
+    }
+
+  private:
+    const unsigned int seed_;
+    std::mt19937 mt_;
+};
 
 template<typename T_state>
 class RandomStateGenerator{};
