b3877d0892eb934e0a46cb59d6ee932a4a195c80
ToruNiina <niina.toru.68u@gmail.com>
2016-08-05 18:52:06 +0900

    add Definition

---
+++ includes/sdl/Definition.hpp
@@ -0,0 +1,13 @@
+#ifndef MINIASCAPE_SDL_RENDERABLE_WINDOW
+#define MINIASCAPE_SDL_RENDERABLE_WINDOW
+#include <SDL2/SDL.h>
+
+namespace miniascape
+{
+
+using Point = SDL_Point;
+using Color = SDL_Color;
+
+}
+
+#endif /* MINIASCAPE_SDL_RENDERABLE_WINDOW */
