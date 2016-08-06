a74b6da59fe10fcdad1130f13172119f7bba5a8a
ToruNiina <niina.toru.68u@gmail.com>
2016-08-06 15:05:58 +0900

    use Window, Renderer instead of RenderableWindow

--- src/ConwaysLifeGameVisualizer.hpp
+++ src/ConwaysLifeGameVisualizer.hpp
@@ -1,7 +1,15 @@
 #include "core/Observer.hpp"
 #include "core/SquareLattice.hpp"
-#include "sdl/RenderableWindow.hpp"
+#include "sdl/Window.hpp"
+#include "sdl/Renderer.hpp"
+#include "sdl/create_window_and_renderer.hpp"
+#include "sdl/window_funcs.hpp"
+#include "sdl/renderer_funcs.hpp"
+#include "sdl/event_funcs.hpp"
 #include "sdl/System.hpp"
+#include "sdl/Definition.hpp"
+#include <memory>
+#include <chrono>
 
 namespace miniascape
 {
@@ -15,45 +23,62 @@ class ConwaysLifeGameVisualizer : public Observer<T_traits>
     using cell_type   = typename base_type::cell_type;
     using time_type   = typename base_type::time_type;
     using world_type  = typename base_type::world_type;
+    using window_type = sdl::Window<std::unique_ptr>;
+    using renderer_type = sdl::Renderer<std::unique_ptr>;
 
   public:
     ConwaysLifeGameVisualizer(const std::size_t x, const std::size_t y)
-        : system_(SDL_INIT_VIDEO), window_(x, y)
+        : system_(SDL_INIT_VIDEO), window_("Conway's Game of Life", 0, 0, x, y,
+          SDL_WINDOW_SHOWN), renderer_(window_, -1, SDL_RENDERER_ACCELERATED)
     {
         const sdl::Color black{0, 0, 0, 0};
-        window_.clear(black);
-        window_.update();
+        sdl::SetDrawColor(this->renderer_, black);
+        sdl::Present(this->renderer_);
     }
-    ~ConwaysLifeGameVisualizer()
-    {
-        window_.~RenderableWindow();
-        system_.~System();
-    }
+    ~ConwaysLifeGameVisualizer() = default;
 
     void observe(const time_type& t, const world_type& world) override;
+    bool closed() const {return closed_;}
 
   private:
-    sdl::System           system_;
-    sdl::RenderableWindow window_;
+    bool          closed_ = false;
+    sdl::System   system_;
+    window_type   window_;
+    renderer_type renderer_;
 };
 
 template<typename T_traits>
 void ConwaysLifeGameVisualizer<T_traits>::observe(
         const time_type& t, const world_type& world)
 {
-    const auto size = window_.size();
+    const auto size = sdl::GetSize(this->window_);
     const sdl::Color green{0, 255, 0, 0};
     const sdl::Color black{0,   0, 0, 0};
-    window_.clear(black);
+    sdl::SetDrawColor(this->renderer_, black);
+    sdl::Clear(renderer_);
+    SquareLattice<T_traits> const& sql =
+        dynamic_cast<SquareLattice<T_traits> const&>(world);
+
     for(int x = 0; x < size.first; ++x)
     for(int y = 0; y < size.second; ++y)
     {
-        sdl::Point pos{x, y};
-        if((dynamic_cast<SquareLattice<T_traits> const&>(world)(x, y))->state)
-            window_.putpixel(pos, green);
+        if(sql(x, y)->state)
+        {
+            sdl::Point pos{x, y};
+            sdl::SetDrawColor(this->renderer_, green);
+            sdl::DrawPoint(this->renderer_, pos);
+        }
     }
-    window_.update();
-    window_.wait(std::chrono::microseconds(10));
+    sdl::Present(this->renderer_);
+
+    using syscl = std::chrono::system_clock;
+    const syscl::time_point until = syscl::now() + std::chrono::milliseconds(1);
+    sdl::Event ev;
+    while(syscl::now() < until)
+    {
+        if(sdl::PollEvent(ev) && ev.type == SDL_QUIT)
+            closed_ = true;
+    }
     return;
 }
 
