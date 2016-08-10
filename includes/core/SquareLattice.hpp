8e1ec627eca18ec2407e13cf55f17273295484c9
ToruNiina <niina.toru.68u@gmail.com>
2016-08-10 16:12:31 +0900

    use boundary in SquareLattice

--- includes/core/SquareLattice.hpp
+++ includes/core/SquareLattice.hpp
@@ -12,16 +12,18 @@ class SquareLattice : public World<T_traits>
 {
   public:
 
-    using traits_type   = T_traits;
-    using size_type     = typename traits_type::size_type;
-    using state_type    = typename traits_type::state_type;
-    using cell_type     = typename traits_type::cell_type;
-    using cell_ptr      = std::unique_ptr<cell_type>;
-    using cell_rawptr   = cell_type*;
-    using boundary_type = typename traits_type::boundary_type;
-    using neighbor_type = typename traits_type::neighbor_type;
+    using traits_type     = T_traits;
+    using size_type       = typename traits_type::size_type;
+    using state_type      = typename traits_type::state_type;
+    using cell_type       = typename traits_type::cell_type;
+    using cell_ptr        = std::unique_ptr<cell_type>;
+    using cell_rawptr     = cell_type*;
+    using boundary_type   = typename traits_type::boundary_type;
+    using neighbor_type   = typename traits_type::neighbor_type;
+    using cell_index_type = typename boundary_type::cell_index_type;
 
     constexpr static std::size_t num_neighbor = neighbor_type::num_neighbor;
+    constexpr static std::size_t dimension    = neighbor_type::dimension;
 
   public:
     SquareLattice() = default;
@@ -33,8 +35,15 @@ class SquareLattice : public World<T_traits>
 
     void randomize(RandomStateGenerator<state_type>& rsg);
 
+    // access element
     cell_ptr &      operator()(const size_type v, const size_type h);
     cell_ptr const& operator()(const size_type v, const size_type h) const;
+    // access element with assert(index is positive)
+    cell_ptr &      operator()(const std::array<int, dimension>&);
+    cell_ptr const& operator()(const std::array<int, dimension>&) const;
+    // access element using the boundary condition
+    cell_ptr &      access(const int v, const int h);
+    cell_ptr const& access(const int v, const int h) const;
 
   protected:
 
@@ -48,20 +57,22 @@ void SquareLattice<T_traits>::create(
         const std::size_t vertical, const std::size_t horizontal)
 {
     vertical_ = vertical; horizontal_ = horizontal;
-    boundary_ = boundary_type({{0,0}}, {{vertical_, horizontal_}});
+    boundary_ = boundary_type({{0,0}}, {{static_cast<int>(vertical_),
+                                         static_cast<int>(horizontal_)}});
 
     // create empty world
     for(std::size_t i = 0; i < (this->vertical_ * this->horizontal_); ++i)
         this->emplace();
 
     // connect neighboring cells
-    for(std::size_t v = 0; v < this->vertical_ ; ++v)
-    for(std::size_t h = 0; h < this->horizontal_ ; ++h)
+    // why int? because neighbor index can be negative while computing
+    for(int v = 0; v < this->vertical_; ++v)
+    for(int h = 0; h < this->horizontal_; ++h)
     {
         for(auto i = 0; i < num_neighbor; ++i)
         {
-            const auto index = boundary_.access(i, {{v, h}});
-            (*this)(v, h)->neighbors.at(i) = (*this)(index[0], index[1]).get();
+            const auto index = neighbor_type::access(i, {{v, h}});
+            (*this)(v, h)->neighbors.at(i) = (boundary_.access(index, *this)).get();
         }
     }
     return;
@@ -102,6 +113,44 @@ SquareLattice<T_traits>::operator()(
     return this->container_.at(v * this->horizontal_ + h);
 }
 
+template<typename T_traits>
+inline typename SquareLattice<T_traits>::cell_ptr&
+SquareLattice<T_traits>::operator()(
+        const std::array<int, dimension>& idx)
+{
+    if(0 <= idx[0] && 0 <= idx[1])
+        return this->container_.at(idx[0] * this->horizontal_ + idx[1]);
+    else
+        throw std::out_of_range("invalid index (" + std::to_string(idx[0]) +
+                                ", " + std::to_string(idx[1]) + ")");
 }
+
+template<typename T_traits>
+inline typename SquareLattice<T_traits>::cell_ptr const&
+SquareLattice<T_traits>::operator()(
+        const std::array<int, dimension>& idx) const
+{
+    if(0 <= idx[0] && 0 <= idx[1])
+        return this->container_.at(idx[0] * this->horizontal_ + idx[1]);
+    else
+        throw std::out_of_range("invalid index (" + std::to_string(idx[0]) +
+                                ", " + std::to_string(idx[1]) + ")");
+}
+
+template<typename T_traits>
+inline typename SquareLattice<T_traits>::cell_ptr const&
+SquareLattice<T_traits>::access(const int h, const int v) const
+{
+    return boundary_.access(cell_index_type{{h, v}}, *this);
+}
+
+template<typename T_traits>
+inline typename SquareLattice<T_traits>::cell_ptr &
+SquareLattice<T_traits>::access(const int h, const int v)
+{
+    return boundary_.access(cell_index_type{{h, v}}, *this);
+}
+
+}//miniascape
 
 #endif /* MINIASCAPE_SQUARE_LATTICE */
